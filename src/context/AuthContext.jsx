66765f75f955caef5fa4167e7abf55ff52ba61b4
Adam Dolphin <Adam.Dolphin@nantltd.local>
2026-08-02 14:28:16 +0100

    refactor: seperate authentication context and provider

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,22 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext } from "react";
 
-const AuthContext = createContext();
+const AuthContext = createContext(null);
 
-export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
-
-    const value = {
-        user,
-        setUser,
-    };
-
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    );
-}
-
-export function useAuth() {
-    return useContext(AuthContext);
-}
+export default AuthContext;
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import AuthContext from "./AuthContext";
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const value = {
+    user,
+    setUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
